09863af7bd2ed8da9d019af28652338530507fc8
vomrat <vomrat@gmail.com>
2018-08-30 13:30:22 +0900

    Complete Tutorial

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from setuptools import setup
 setup(name='myfirstmodule',
     version='0.1',
     packages=['myfirstmodule'],
+    install_requires=[
+        'markdown',
+    ],
     zip_safe=False,
+    entry_points = {
+        'console_scripts': ['myfirstmodule=myfirstmodule.command_line:main']
+    },
+    include_package_data=True,
 )
